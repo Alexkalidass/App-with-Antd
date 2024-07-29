dbb585a78fa9676168e9fac21ea1551542de2435
Alexkalidass <akhiraservicess01@gmail.com>
2024-07-29 21:22:37 +0530

    Applictaion, Slider and Crud functions File Added

--- React-task/src/App.jsx
+++ React-task/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
-function AppRouter() {
+function App() {
   return (
     <Router>
       <Routes>
@@ -14,4 +14,5 @@ function AppRouter() {
   );
 }
 
-export default AppRouter;
+export default App;
+
